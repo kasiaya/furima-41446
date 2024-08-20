a27f8b05b26f367a80786b0fd52ec5fbb958a28f
kasiaya <ayano.kashimoto@gmail.com>
2024-08-20 20:49:19 +0900

    アカウント管理

--- Gemfile
+++ Gemfile
@@ -79,4 +79,5 @@ group :production do
   gem 'pg'
 end
 
+gem 'pry-rails'
 gem 'devise'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
 
   private
 
@@ -7,5 +9,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD'] # 環境変数を読み込む記述に変更
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
   end
 end
