da31a903c13f884ad766868ee006663b156b241d
kasiaya <ayano.kashimoto@gmail.com>
2024-08-21 16:00:58 +0900

    バリデーション追加_全角

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name, presence: true,
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々〆〤]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :last_name, presence: true,
+                        format: { with: /\A[ぁ-んァ-ヶ一-龥々〆〤]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :first_name_kana, presence: true,
+                              format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana character' }
+  validates :last_name_kana, presence: true,
+                             format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana character' }
   validates :birth_date, presence: true
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'must contain both letters and numbers' }
 end
