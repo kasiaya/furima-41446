cc4355e43e354779e4e1f3962bdc806bc4dbbac6
kasiaya <ayano.kashimoto@gmail.com>
2024-08-22 16:01:49 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,14 @@ class Item < ApplicationRecord
   belongs_to :delivery_payer
   belongs_to :delivery_area
   belongs_to :lead_time
+
+  # バリデーション
+  validates :item_name, presence: true
+  validates :item_description, presence: true
+  validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :delivery_payer_id, numericality: { other_than: 1 }
+  validates :delivery_area_id, numericality: { other_than: 1 }
+  validates :lead_time_id, numericality: { other_than: 1 }
 end
