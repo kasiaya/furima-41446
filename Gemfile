f2bdd75304b92deedb16a808f5841807f6450e9a
kasiaya <ayano.kashimoto@gmail.com>
2024-08-22 15:54:02 +0900

    ActiveHashの導入

--- Gemfile
+++ Gemfile
@@ -87,3 +87,5 @@ gem 'devise'
 
 gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
+
+gem 'active_hash'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  # ActiveHashの設定とアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_payer
+  belongs_to :delivery_area
+  belongs_to :lead_time
 end
