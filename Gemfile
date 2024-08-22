6887a41d65b7874d341f6523d676edcc043c3e4f
kasiaya <ayano.kashimoto@gmail.com>
2024-08-22 14:21:46 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -84,3 +84,6 @@ end
 
 gem 'pry-rails'
 gem 'devise'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,15 @@ class ItemsController < ApplicationController
   def new
     # @items = Item.new
   end
+
+  def create
+    Item.create(item_params)
+    # redirect_to '/'
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
