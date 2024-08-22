4632e97fff6fe6dc5809a5749ac75cb117456267
kasiaya <ayano.kashimoto@gmail.com>
2024-08-22 18:29:58 +0900

    商品情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,25 @@
 class ItemsController < ApplicationController
   def index
-    # @items = Item.all
+    @items = Item.all
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
-    Item.create(item_params)
-    # redirect_to '/'
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_description, :price, :category_id, :condition_id, :delivery_payer_id,
+                                 :delivery_area_id, :lead_time_id).merge(user_id: current_user.id)
   end
 end
