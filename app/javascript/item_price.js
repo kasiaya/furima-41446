e0b0911fb31533a0d068106f2e64857c32a07a38
kasiaya <ayano.kashimoto@gmail.com>
2024-08-23 21:56:40 +0900

    turbo:renderのイベントを指定

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,6 @@
-window.addEventListener('turbo:load', () => {
+const price = () => {
+
+
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
@@ -9,5 +11,10 @@ window.addEventListener('turbo:load', () => {
     const profitDom = document.getElementById("profit");
     profitDom.innerHTML = Math.floor(inputValue-Math.floor(priceInput.value*0.1));
  })
-});
+
+
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
 
