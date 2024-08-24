3b8bdab6a00ae3687d25a390bc6dea823d43c08f
kasiaya <ayano.kashimoto@gmail.com>
2024-08-24 19:03:02 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    # @items = Item.all
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
