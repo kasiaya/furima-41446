6a88e09f76b5ec79ea254a0f798065df2289deff
kasiaya <ayano.kashimoto@gmail.com>
2024-08-25 16:04:02 +0900

    編集画面への変遷

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,16 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to root_path
+  end
+
   private
 
   def item_params
