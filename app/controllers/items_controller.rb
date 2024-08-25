eee5b6de1fcf9ad41adab06ce65efb33cea04f0d
kasiaya <ayano.kashimoto@gmail.com>
2024-08-25 17:23:40 +0900

    エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,9 +26,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to root_path
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
