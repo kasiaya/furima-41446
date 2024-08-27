beb8e8fafff456a4fb03eff6d1cabf468be5709d
kasiaya <ayano.kashimoto@gmail.com>
2024-08-27 14:10:52 +0900

    コントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,11 @@
 class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @orders = @item.orders
+  end
+
+  def create
+    # @item = Item.find(params[:item_id])
+    # @order = @item.orders.build(order_params)
+  end
 end
