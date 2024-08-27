a8e052fcae1234ab41e067432b473c6a7964d113
kasiaya <ayano.kashimoto@gmail.com>
2024-08-27 18:21:03 +0900

    エラーメッセージ

--- app/models/order_delivery_address.rb
+++ app/models/order_delivery_address.rb
@@ -6,6 +6,7 @@ class OrderDeliveryAddress
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :delivery_area_id, numericality: { other_than: 1 }
     validates :city
     validates :street_number
 
