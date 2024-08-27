1da00e7c0c7bbf5c93c75923d07b39689f4e191f
kasiaya <ayano.kashimoto@gmail.com>
2024-08-27 18:10:37 +0900

    フォームの情報を保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,26 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @orders = @item.order
+    @order_delivery_address = OrderDeliveryAddress.new
   end
 
   def create
-    # @item = Item.find(params[:item_id])
-    # @order = @item.orders.build(order_params)
+    @item = Item.find(params[:item_id])
+    @order_delivery_address = OrderDeliveryAddress.new(order_params)
+    # binding.pry
+    if @order_delivery_address.valid?
+      @order_delivery_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_delivery_address).permit(:postal_code, :delivery_area_id, :city, :street_number, :building_name,
+                                                   :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
