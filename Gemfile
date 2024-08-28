549db4e5bfe3ea7a0758dec206c90749d073daa1
kasiaya <ayano.kashimoto@gmail.com>
2024-08-28 16:25:40 +0900

    決済機能の実装

--- Gemfile
+++ Gemfile
@@ -89,3 +89,4 @@ gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
 
 gem 'active_hash'
+gem 'payjp'
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,13 @@ class OrdersController < ApplicationController
     @order_delivery_address = OrderDeliveryAddress.new(order_params)
     # binding.pry
     if @order_delivery_address.valid?
+      Payjp.api_key = 'sk_test_321bb7c5c5c45cacb06cbfc7' # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      price = @item.price
+      Payjp::Charge.create(
+        amount: price, # 商品の値段
+        card: order_params[:token], # カードトークン
+        currency: 'jpy' # 通貨の種類（日本円）
+      )
       @order_delivery_address.save
       redirect_to root_path
     else
@@ -21,6 +28,6 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_delivery_address).permit(:postal_code, :delivery_area_id, :city, :street_number, :building_name,
-                                                   :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+                                                   :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
